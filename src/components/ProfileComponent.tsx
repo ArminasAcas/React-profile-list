a794b46847774933d174818392f49855f837cbff
ArminasAcas <arminasprollas@gmail.com>
2023-08-27 21:37:45 +0300

    Profile css improvements

--- src/components/ProfileComponent.tsx
+++ src/components/ProfileComponent.tsx
@@ -11,13 +11,12 @@ export default function Profile(props: ProfileData) {
 
     return (
         <div className="profile">
-            <div className="profile__section">
+            <div className="profile__section--60">
                 <img src={props.profileImageUrl} className="profile__image"></img>
                 <span className="profile__information">{props.username}</span>
             </div>
-
-            <span className="profile__information">{props.profession}</span>
-            <span className="profile__information">{props.age}</span>
+                <span className="profile__section--30">{props.profession}</span>
+                <span className="profile__section--10">{props.age}</span>
         </div>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ProfilePage from './pages/ProfilePage.tsx'
+import "./css/ProfilePage.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ProfilePage />
+      <ProfilePage />
   </React.StrictMode>,
 )
+
+let page = document.getElementById('root');
+if (page) page.classList.add("page");
