d90ad51b44163393e000ac9e3aa3bb5f27b0597b
ArminasAcas <arminasprollas@gmail.com>
2023-08-30 23:51:14 +0300

    added the ability to hide unemployed users

--- src/components/ProfileListComponent.tsx
+++ src/components/ProfileListComponent.tsx
@@ -3,6 +3,7 @@ import Divider from "../components/DividerComponent"
 import { profiles } from "../globals/DummyData"
 import "../css/ProfileListComponent.css"
 import { useState } from 'react';
+import { useEffect } from "react";
 
 export default function ProfileList() {
 
@@ -10,45 +11,75 @@ export default function ProfileList() {
     const [sortByProffesion, setSortByProfession] = useState(0);
     const [sortByUsername, setSortByUsername] = useState(0);
     const [activeButton, setActiveButton] = useState(-1);
-
-    let sortedProfiles = profiles;
+    const [hideUnemployedUsers, setHideUnemployedUsers] = useState(false);
+    const [sortedProfiles, setSortedProfiles] = useState(profiles);
 
     function handleAgeClick() {
         if (sortByAge === 0) setSortByAge(1);
         else setSortByAge(0);
         
-        if (sortByAge === 1) sortedProfiles.sort((a,b) => a.age - b.age);
-        else sortedProfiles.sort((a,b) => b.age - a.age);
+        let newSortedProfiles;
+        if (sortByAge === 1) newSortedProfiles = sortedProfiles.sort((a,b) => a.age - b.age);
+        else newSortedProfiles = sortedProfiles.sort((a,b) => b.age - a.age);
         setActiveButton(2);
+        setSortedProfiles(newSortedProfiles);
+        if (hideUnemployedUsers) filterUnempoyedUsers();
      }
 
      function handleProfessionClick() {
         if (sortByProffesion === 0) setSortByProfession(1);
         else setSortByProfession(0);
 
-        if (sortByProffesion === 1) sortedProfiles.sort((a, b) => b.profession.localeCompare(a.profession));
-        else sortedProfiles.sort((a, b) => a.profession.localeCompare(b.profession));
+        let newSortedProfiles;
+        if (sortByProffesion === 1) newSortedProfiles = sortedProfiles.sort((a, b) => b.profession.localeCompare(a.profession));
+        else newSortedProfiles = sortedProfiles.sort((a, b) => a.profession.localeCompare(b.profession));
         setActiveButton(1);
+        setSortedProfiles(newSortedProfiles);
+        if (hideUnemployedUsers) filterUnempoyedUsers();
      }
 
      function handleUsernameClick() {
         if (sortByUsername === 0) setSortByUsername(1);
         else setSortByUsername(0);
 
-        if (sortByUsername === 1) sortedProfiles.sort((a,b) => b.username.localeCompare(a.username));
-        else sortedProfiles.sort((a,b) => a.username.localeCompare(b.username));
+        let newSortedProfiles;
+        if (sortByUsername === 1) newSortedProfiles = sortedProfiles.sort((a,b) => b.username.localeCompare(a.username));
+        else newSortedProfiles = sortedProfiles.sort((a,b) => a.username.localeCompare(b.username));
         setActiveButton(0);
+        setSortedProfiles(newSortedProfiles);
+        if (hideUnemployedUsers) filterUnempoyedUsers();
      }
 
+     function handleHideClick() {
+        setHideUnemployedUsers(!hideUnemployedUsers);
+     }
+
+     function filterUnempoyedUsers() {
+        let newSortedPrifles = sortedProfiles.filter( profile => {
+            return profile.profession !== "Unemployed";
+        })
+        setSortedProfiles(newSortedPrifles);
+     }
+
+     useEffect(() => {
+        if (hideUnemployedUsers) {
+            filterUnempoyedUsers();
+        } else {
+            setSortedProfiles(profiles);
+        }
+    }, [hideUnemployedUsers]);
 
      let profileList =  sortedProfiles.map(profile => 
         <Profile key={profile.id} profileImageUrl={profile.imageUrl}  username={profile.username} profession={profile.profession}  age={profile.age} />
-    ) 
+    )
     
     return (
         <>
             <div className="profile-list">
                 <div className="profile-list__text">
+                    <input className="profile-list__search" type="text" placeholder="enter username"></input>
+                    <input className="profile-list__checkbox" type="checkbox" onClick={handleHideClick}></input>
+                    <label> Hide unemployed users</label>
                     <button className={`profile-list__text--60 profile-list__button ${activeButton === 0 ? 'profile-list__button--active' : ''} `} onClick={handleUsernameClick}>User</button>
                     <button className={`profile-list__text--30 profile-list__button ${activeButton === 1 ? 'profile-list__button--active' : ''} `} onClick={handleProfessionClick}>Profession</button>
                     <button className={`profile-list__text--10 profile-list__button ${activeButton === 2 ? 'profile-list__button--active' : ''} `} onClick={handleAgeClick}>Age</button>
